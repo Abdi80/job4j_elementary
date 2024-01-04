442abd95cc8c7c9415f861e1d4070d5d725610a1
Nurlan <001nurlan@gmail.com>
2024-01-04 15:50:03 +0600

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Nurlan");
+        System.out.println("23.03.1978");
+    }
+}
