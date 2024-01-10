ba2b897c640ee2fdd04bb3e687a2d625e3d79673
Nurlan <001nurlan@gmail.com>
2024-01-10 19:35:07 +0600

    4.2. Тернарное сравнение [#189]

---
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MaxTest {
+
+    @Test
+    void whenMax1To7Then7() {
+        int left = 1;
+        int right = 7;
+        int result = Max.max(left, right);
+        int expected = 7;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax89To5Then89() {
+        int left = 89;
+        int right = 5;
+        int result = Max.max(left, right);
+        int expected = 89;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax4To4Then4() {
+        int left = 4;
+        int right = 4;
+        int result = Max.max(left, right);
+        int expected = 4;
+        assertThat(result).isEqualTo(expected);
+    }
+}
